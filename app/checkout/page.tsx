f82535da3b67d98a07db77c009dd07dd0666f73f
vishal kumar <vishalkumar437@gmail.com>
2023-10-02 17:24:14 +0530

    added cod

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -7,35 +7,45 @@ import { Box, Button } from "@mui/material";
 import axios from "axios";
 import { useSelector } from "react-redux";
 import { AppState } from "../interface/interface";
-export default function Checkout() {
-  const [address,setAddress] = useState({});
-  const userId = useSelector((state:AppState)=>state.auth.userInfo?.id);
-  const [paymentMethod,setPaymentMethod] = useState('COD'); 
+const Checkout = () => {
+  const [address, setAddress] = useState({});
+  const userId = useSelector((state: AppState) => state.auth.userInfo?.id);
+  const [paymentMethod, setPaymentMethod] = useState("COD");
   const urlParams = new URLSearchParams(window.location.search);
   const cartId = urlParams.get("id");
-  const handleSubmit = async ()=>{
+  const handleSubmit = async () => {
     const data = {
-      address:{...address,userId:userId},
-      paymentMethod:paymentMethod,
-      cartId:cartId
+      address: { ...address, userId: userId },
+      paymentMethod: paymentMethod,
+      cartId: cartId,
+    };
+    if (paymentMethod === "COD") {
+      const response = await axios.post("http://localhost:3000/newOrder", data);
     }
-    if(paymentMethod==='COD'){
-      const response = await axios.post("http://localhost:3000/newOrder",data);
-    }
-  }
+  };
   return (
     <div style={{ padding: "10px", margin: "10px" }}>
       <CheckoutCards cardType="Address">
-        <DeliveryForm set={setAddress}/>
+        <DeliveryForm set={setAddress} />
       </CheckoutCards>
       <CheckoutCards cardType="Payment">
-        <CardPaymentDetails set={setPaymentMethod} paymentMethod={paymentMethod}/>
+        <CardPaymentDetails
+          set={setPaymentMethod}
+          paymentMethod={paymentMethod}
+        />
       </CheckoutCards>
-      <Box sx={{textAlign:"center"}}>
-        <Button variant="contained" color="primary" size="large" type="submit" onClick={handleSubmit}>     
+      <Box sx={{ textAlign: "center" }}>
+        <Button
+          variant="contained"
+          color="primary"
+          size="large"
+          type="submit"
+          onClick={handleSubmit}
+        >
           Make Payment
         </Button>
       </Box>
     </div>
   );
-}
+};
+export default Checkout;
